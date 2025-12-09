8125c51be6c045cc80a62d1b2acabc3a0bc7f88a
falba3 <99144557+falba3@users.noreply.github.com>
2025-12-10 00:38:48 +0100

    access-control-allow-origin for API routes

--- next.config.js
+++ next.config.js
@@ -3,6 +3,19 @@ const nextConfig = {
   images: {
     domains: ["deanna-banana-main-bananabucketdev.s3.us-east-1.amazonaws.com"],
   },
+  async headers() {
+    return [
+      {
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" }, // Adjust this to specific domains in production
+          { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
+          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+        ]
+      }
+    ]
+  }
 };
 
 export default nextConfig;
