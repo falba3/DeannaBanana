799a09860c05d46eef256bfed07ebb22cd38ab98
falba3 <99144557+falba3@users.noreply.github.com>
2026-01-08 18:15:43 +0100

    Fixing CORS

--- next.config.js
+++ next.config.js
@@ -8,7 +8,7 @@ const nextConfig = {
       {
         source: "/api/:path*",
         headers: [
-          { key: "Access-Control-Allow-Credentials", value: "true" },
+          // { key: "Access-Control-Allow-Credentials", value: "true" }, // Removed as it conflicts with Access-Control-Allow-Origin: "*"
           { key: "Access-Control-Allow-Origin", value: "*" }, // Adjust this to specific domains in production
           { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
           { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
