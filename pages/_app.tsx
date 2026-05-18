fc1193b7c33b5c6f76ce570a577f1bde71cdaba8
falba3 <99144557+falba3@users.noreply.github.com>
2026-05-18 12:05:30 +0200

    changed icon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,7 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { Toaster } from '@/components/ui/toaster';
 import { Toaster as Sonner } from '@/components/ui/sonner';
 import { TooltipProvider } from '@/components/ui/tooltip';
+import Head from 'next/head';
 
 import "../styles/globals.css"; // Import global styles
 
@@ -12,6 +13,9 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <QueryClientProvider client={queryClient}>
       <TooltipProvider>
+        <Head>
+          <link rel="icon" href="/deanna2u.ico" />
+        </Head>
         <Toaster />
         <Sonner />
         <Component {...pageProps} />
